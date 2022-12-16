3718e3730228bbb9f3b932721ead10009af3339f
Wrisssttt <104536444+Wrisssttt@users.noreply.github.com>
2022-12-17 00:49:54 +0200

    Add files via upload

---
+++ LR5/1level(example)/4/script.js
@@ -0,0 +1,17 @@
+function clearTextFromHTMLTags(textForEditing)
+{
+var tagTemplate = new RegExp("</?[a-zA-Z]{1,}/?>","g");
+return textForEditing.replace(tagTemplate,"");
+}
+<textarea rows="20" cols="20" id="editor">
+<html>
+<i>some text<i>
+<b>some text</i>
+<br>
+<br>
+<br>
+</html>
+</textarea >
+<input type="button" value="Clear text" onclick="document.getElementById('editor').
+value=clearTextFromHTMLTags(document.getElementById('editor
+').value);" >
